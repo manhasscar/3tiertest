3d5044eef9cc1d3860682f2ff66c67b502e486c0
박경섭 <ParkKyoungSeop@park-MacBookPro.local>
2023-04-04 17:05:21 +0900

    test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import { useEffect, ReactDOM, React, useState } from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [hi, setHi] = useState(0);
+  useEffect(() => {
+    axios.get("http://118.67.155.184:8080/api/test").then((res) => {
+      setHi(res.data);
+    });
+  });
+  return <div className="App">{hi}</div>;
 }
 
 export default App;
